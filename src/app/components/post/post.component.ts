45389f632fb7584029f18865ba28b73d78f6182d
mato073 <gaspard.mater@epitech.eu>
2021-02-06 16:59:02 +0100

    [MOD] In Component post print the post byID

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Blog } from "../../models/blog.model";
+
+import { Router, ActivatedRoute } from "@angular/router";
+import { BlogService} from '../../services/blog.service'
+
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-post',
@@ -7,9 +13,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  post?: any;
+  CardSubscription?: Subscription;
 
-  ngOnInit(): void {
+  constructor(private blogService: BlogService,
+    private route:  ActivatedRoute,) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+
+    this.blogService.getSingleBlog(+id).then(
+      (post: any) => {
+        this.post = post;
+        console.log('post =', this.post);
+        
+      }
+    )
   }
-
 }
