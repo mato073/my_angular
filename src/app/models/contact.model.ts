de7b4be6ab1e912fc63dcdc43cc21b547f1dda86
Ronan Bouziane <42178526+rbouziane@users.noreply.github.com>
2021-02-07 15:51:02 +0100

    [MOD] contact model to supress country

--- src/app/models/contact.model.ts
+++ src/app/models/contact.model.ts
@@ -1,4 +1,4 @@
 export class Contact {
-  constructor(public firstName: string, public lastName: string, public country: string, public content: string) {
+  constructor(public firstName: string, public lastName: string, public content: string) {
   }
 }
